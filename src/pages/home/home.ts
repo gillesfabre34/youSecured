a171548088f9a6f55ed54e2dbc22e50adb3a5883
gillesfabre34 <gillesfabre34@gmail.com>
2017-07-09 14:32:09 +0200

    Added ngIfs to home page for signup form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,12 +2,24 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor() {
-  }
-
+	addedContact: boolean;
+	signup: boolean;
+	
+	constructor() {
+		this.addedContact = false;
+		this.signup = false;
+	}
+	
+	submitSignUp() {
+		this.signup = true;
+	}
+	
+	submitAddContact() {
+		this.addedContact = true;
+	}
+	
 }
