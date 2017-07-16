421408068280fe2490051e5be4b8779967210e59
gillesfabre34 <gillesfabre34@gmail.com>
2017-07-16 16:47:43 +0200

    Still don't understand how to use FB credentials

--- src/app/facebook.ts
+++ src/app/facebook.ts
@@ -2,6 +2,7 @@
  * Created by gilles on 16/07/17.
  */
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import * as firebase from 'firebase/app';
 
 declare const FB: any;
 
@@ -19,21 +20,53 @@ export class Facebook {
 	
 	login(): Promise<any> {
 		// return Promise.resolve(() => {
-			return FB.login(function (response) {
-				if (response.authResponse) {
-					console.log('Welcome!  Fetching your information.... ');
-					FB.api('/me', {fields: 'name,email'}, function (response) {
-						console.log("%cFacebook login response ", "color: blue; font-weight:bold;", response);
-						console.log('%cHello, ' + response.name + ' !', "color: blue; font-weight:bold;","");
-						console.log('%cemail : ' + response.email, "color: blue; font-weight:bold;","");
-						return response;
+		return FB.login(function (response) {
+			if (response.authResponse) {
+				console.log("%cIndex fbAsyncInit response : ","font-weight: bold; color:blue;",response);
+				if(response.status === 'connected') {
+					console.log("%cCONNECTED ! ","font-weight: bold; color:green;",response);
+					let uid = response.authResponse.userID;
+					let accessToken = response.authResponse.accessToken;
+					console.log("%cuid ","font-weight: bold; color:green;",uid);
+					console.log("%caccessToken ","font-weight: bold; color:green;",accessToken);
+					firebase.auth.FacebookAuthProvider.credential(accessToken);
+					FB.api('/me', function(response) {
+						console.log(JSON.stringify(response));
+					});
+				} else if (response.status === 'not_authorized') {
+					console.log("%cLOGGED IN TO FB BUT NOT AUTHENTICATED APP ! ","font-weight: bold; color:orange;",response);
+					// the user is logged in to Facebook,
+					// but has not authenticated your app
+					FB.login(function(response) {
+						if (response.authResponse) {
+							console.log('Welcome!  Fetching your information.... ');
+							FB.api('/me', function(response) {
+								console.log('Good to see you, ' + response.name + '.');
+							});
+						} else {
+							console.log('User cancelled login or did not fully authorize.');
+						}
 					});
 				} else {
-					console.log('User cancelled login or did not fully authorize.');
+					console.log("%NOT LOGGED IN TO FB ! ","font-weight: bold; color:orange;",response);
+					// the user isn't logged in to Facebook.
 				}
-			},
-				{scope: 'public_profile,email'});
-		// });
+				
+				console.log('Welcome!  Fetching your information.... ');
+				FB.api('/me', {fields: 'name,email'}, function (response) {
+					console.log("%cFacebook login response ", "color: blue; font-weight:bold;", response);
+					console.log('%cHello, ' + response.name + ' !', "color: blue; font-weight:bold;","");
+					console.log('%cemail : ' + response.email, "color: blue; font-weight:bold;","");
+					let credential = firebase.auth.FacebookAuthProvider.credential(response.email);
+					let auth = firebase.auth();
+					let currentUser = auth.currentUser;
+					return response;
+				});
+			} else {
+				console.log('User cancelled login or did not fully authorize.');
+			}
+		},
+		{scope: 'public_profile,email'});
 	}
 	
 	init() {
