bd9db38431706dd14ff57055a6407fa8c9c24f9c
gillesfabre34 <gillesfabre34@gmail.com>
2017-07-16 13:54:45 +0200

    Trying to add correct CSS to FB login button

--- src/app/facebook.ts
+++ src/app/facebook.ts
@@ -22,7 +22,7 @@ export class Facebook {
 			if (response.authResponse) {
 				console.log('Welcome!  Fetching your information.... ');
 				FB.api('/me', function(response) {
-					console.log('Good to see you, ' + response.name + '.');
+					console.log('Hello, ' + response.name + ' !');
 				});
 			} else {
 				console.log('User cancelled login or did not fully authorize.');
